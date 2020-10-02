e31f7eff3ded990a721159ac20f3eb230c75f891
Peltax <>
2020-10-03 01:15:18 +0300

    project skeleton

---
+++ app/src/main/java/com/peltax/dimple/HomeFragment.kt
@@ -0,0 +1,23 @@
+package com.peltax.dimple
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.navGraphViewModels
+
+class HomeFragment : Fragment() {
+
+    private val viewModel: HomeViewModel by navGraphViewModels(R.id.nav_graph) {
+        defaultViewModelProviderFactory
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.home_fragment, container, false)
+    }
+
+}
